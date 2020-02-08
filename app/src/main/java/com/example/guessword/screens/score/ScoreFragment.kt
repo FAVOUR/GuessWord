5dc945ff0f7e84a3ebcf58cd3757874b5ba74b16
FavourItex <Favour.olije@iisysgroup.com>
2020-02-08 12:59:23 +0100

    Project and score fragment have been implemented

--- app/src/main/java/com/example/guessword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/guessword/screens/score/ScoreFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.example.guessword.R
+import com.example.guessword.databinding.FragmentScoreBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -15,12 +17,23 @@ import com.example.guessword.R
 class ScoreFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false)
+
+        // Inflate view and obtain an instance of the binding class.
+        val binding: FragmentScoreBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_score,
+            container,
+            false
+        )
+
+        return binding.root
     }
-
-
 }
+
+
+
+
